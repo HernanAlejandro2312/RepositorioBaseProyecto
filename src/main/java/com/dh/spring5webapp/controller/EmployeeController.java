602346841de90129edf364d78e14d46ea0a97c4d
Edson Terceros <Edson.Terceros@agilysys.com>
2018-04-12 19:19:13 -0400

    Employee controller

---
+++ src/main/java/com/dh/spring5webapp/controller/EmployeeController.java
@@ -0,0 +1,26 @@
+/**
+ * @author: edson
+ */
+
+package com.dh.spring5webapp.controller;
+
+import com.dh.spring5webapp.repositories.EmployeeRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class EmployeeController {
+    private EmployeeRepository employeeRepository;
+
+    public EmployeeController(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+
+    @RequestMapping("/employees")
+    public String getEmployees(Model model) {
+        model.addAttribute("employee", employeeRepository.findAll());
+        return "employees";
+    }
+}
