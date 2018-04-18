8f166c761864abff0403de812f5a6f0399d7e686
Edson Terceros <Edson.Terceros@agilysys.com>
2018-04-18 15:03:00 -0400

    application error handling and page

---
+++ src/main/java/com/dh/spring5webapp/controller/ControllerExceptionHandler.java
@@ -0,0 +1,23 @@
+/**
+ * (C) 2017 Agilysys NV, LLC.  All Rights Reserved.  Confidential Information of Agilysys NV, LLC.
+ */
+package com.dh.spring5webapp.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public ModelAndView handleNumberFormat(Exception e) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("applicationerror");
+        modelAndView.addObject("exception", e);
+        return modelAndView;
+    }
+}
