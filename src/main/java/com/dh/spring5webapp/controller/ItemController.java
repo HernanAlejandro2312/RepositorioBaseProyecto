df9bc771d6022fda1726aad7248557b734b49e6d
Edson Terceros <Edson.Terceros@agilysys.com>
2018-04-18 14:57:04 -0400

    specific error page

--- src/main/java/com/dh/spring5webapp/controller/ItemController.java
+++ src/main/java/com/dh/spring5webapp/controller/ItemController.java
@@ -4,15 +4,18 @@
 
 package com.dh.spring5webapp.controller;
 
+import com.dh.spring5webapp.exceptions.NotFoundException;
 import com.dh.spring5webapp.model.Item;
 import com.dh.spring5webapp.model.SubCategory;
 import com.dh.spring5webapp.services.ItemService;
 import com.dh.spring5webapp.services.SubCategoryService;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.constraints.NotNull;
@@ -103,6 +106,15 @@ public class ItemController {
             InputStream is = new ByteArrayInputStream(byteArray);
             IOUtils.copy(is, response.getOutputStream());
         }
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound(Exception e) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("error");
+        modelAndView.addObject("exception", e);
+        return modelAndView;
     }
 
 }    
